b11540eee56d900c7e1f64dd8f2b28412d51d3f0
Arthur Luiz Silva Correia <arthur.correia@aramis.local>
2026-05-14 11:07:37 -0300

    fix: adjust CORS for production compatibility

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ _origins = (
 app.add_middleware(
     CORSMiddleware,
     allow_origins=_origins,
-    allow_credentials=True,
+    allow_credentials=False,  # Necessário False para funcionar com '*' em muitos browsers
     allow_methods=["*"],
     allow_headers=["*"],
 )
